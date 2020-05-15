c2a2e04d360d2fdbe64e10dc40adc2716bafdabc
JoseLRiano <joselriano@yahoo.com>
2020-05-15 02:31:11 -0400

    Updates

---
+++ precache-manifest.72169fa3ffbbea402f06d050ba15c9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60ea561b1bc487a9240c0d8de55f3a9",
+    "url": "/ReactDice/index.html"
+  },
+  {
+    "revision": "4395ecd71679d8e251f3",
+    "url": "/ReactDice/static/css/main.74b12bca.chunk.css"
+  },
+  {
+    "revision": "b0a997ad90e390319c46",
+    "url": "/ReactDice/static/js/2.8f933600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDice/static/js/2.8f933600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4395ecd71679d8e251f3",
+    "url": "/ReactDice/static/js/main.58311461.chunk.js"
+  },
+  {
+    "revision": "c0c73892b5b9ef2e01cc",
+    "url": "/ReactDice/static/js/runtime-main.84904325.js"
+  }
+]);
